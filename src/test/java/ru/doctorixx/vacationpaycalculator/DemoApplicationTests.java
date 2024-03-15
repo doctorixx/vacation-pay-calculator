eeb248f195f045351631ac34bef53ac97119f4d0
Doctorixx <61980858+Windows-up@users.noreply.github.com>
2024-03-15 23:49:10 +0300

    Initial commit

---
+++ src/test/java/ru/doctorixx/vacationpaycalculator/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.doctorixx.vacationpaycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
